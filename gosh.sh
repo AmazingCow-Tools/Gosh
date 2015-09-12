43d025f4d536aaa3de822bbee0da09c0d2b8e74f
n2omatt <n2o.matt@gmail.com>
2015-09-12 11:07:26 -0300

    Modify the list function to print the list of bookmarks in an more formatted way. Add option to print the list of bookmarks in colors or keep the plain output. Update the help message. Update the program version.

--- gosh.sh
+++ gosh.sh
@@ -50,26 +50,28 @@ gosh() (
     PATH_TEMP_FILE="${PATH_DIR_RC}"/temp_goshrc.txt
 
     BOOKMARK_SEP=":"
+    NO_COLORS=false
 
     ############################################################################
     ## Helper Functions                                                       ##
     ############################################################################
-    show_help() { 
+    show_help() {
         echo "Usage:"
-        echo "  gosh [-hv] [-l] [-aru] ..."
+        echo "  gosh [-hv] [-ln] [-aru] ..."
         echo "  gosh-go <bookmark>"
         echo ""
-        echo "  -h : Show this screen."
-        echo "  -v : Show app version and copyright."
+        echo "  -h                   : Show this screen."
+        echo "  -v                   : Show app version and copyright."
         echo "  -a <Bookmark> <Path> : Add a Bookmark with specified path."
         echo "  -r <Bookmark>        : Remove a Bookmark."
         echo "  -u <Bookmark> <Path> : Update a Bookmark to path."
-        echo "  -l : Show all Bookmarks and Paths."
+        echo "  -l                   : Show all Bookmarks and Paths."
+        echo "  -n                   : Print the output without colors."
         echo ""
-        echo "  gosh-go - Change dir to Bookmark's path."
+        echo "  gosh-go <bookmark> - Change dir to Bookmark's path."
     }
     show_version() {
-        echo "gosh - 0.1 - N2OMatt <n2omatt@amazingcow.com>"
+        echo "gosh - 0.1.1 - N2OMatt <n2omatt@amazingcow.com>"
         echo "Copyright (c) 2015 - Amazing Cow"
         echo "This is a free software (GPLv3) - Share/Hack it"
         echo "Check opensource.amazingcow.com for more :)"
@@ -78,7 +80,7 @@ gosh() (
     {
         echo "ERROR: ${1}";
     }
-    
+
     #File/Dir Functions.
     check_dir_and_files() {
         #Check if the rc folder exists.
@@ -87,21 +89,21 @@ gosh() (
             mkdir -p "${PATH_DIR_RC}"
         fi
         #Check if the file containing the data exists.
-        if [ ! -e "${PATH_FILE_RC}" ]; then 
+        if [ ! -e "${PATH_FILE_RC}" ]; then
             print_error "File ${PATH_FILE_RC} not found - creating one now."
             touch "${PATH_FILE_RC}"
-        fi  
+        fi
     }
     sort_file() {
         cat "${PATH_FILE_RC}" | sort > "${PATH_TEMP_FILE}";\
         mv "${PATH_TEMP_FILE}" ${PATH_FILE_RC}
     }
-    
+
     #Bookmark Functions.
     bookmark_exists() {
         #Check if already have a bookmark with the name.
         name=$1
-        if [[ -z $name ]]; then 
+        if [[ -z $name ]]; then
             return 1;
         fi
 
@@ -128,7 +130,59 @@ gosh() (
     ## Action Functions                                                       ##
     ############################################################################
     list() {
-        cat $PATH_FILE_RC
+        file_contents=$(cat $PATH_FILE_RC);
+
+        #Turn the contents of file into an array without the : chars.
+        array=(${file_contents//:/ });
+
+        #Bookmark that has the greater name.
+        greater_length=0;
+
+        #Find the length of the name of the bookmark that has the longer name.
+        for index in "${!array[@]}"
+        do
+            #Bookmarks are located in even indexes.
+            if [ $((index % 2)) -eq 0 ]; then
+
+                value=${array[index]};    #Value of the array at index.
+                current_length=${#value}; #Lemgth of this bookmark name.
+                #Update the length if needed.
+                if [ $current_length -gt $greater_length ]; then
+                    greater_length=$current_length;
+                fi
+            fi
+        done
+
+        #Print the bookmarks...
+        for index in "${!array[@]}"
+        do
+            value=${array[index]}; #Current value of the array.
+
+            #Names are located in even indexes, paths in odd indexes.
+            if [ $((index % 2)) -eq 0 ]; then
+                current_length=${#value};
+                fmt="%-0$greater_length""s";
+
+                #Put colors...
+                if [ $NO_COLORS = false ]; then
+                    tput setf 2;
+                fi;
+
+                #Print the bookmark name.
+                printf $fmt $value;
+
+                #Put colors...
+                if [ $NO_COLORS = false ]; then
+                    tput sgr0;
+                fi;
+
+                #Print the separator.
+                printf " : ";
+
+            else
+                echo $value;
+            fi
+        done
     }
 
     add() {
@@ -136,7 +190,7 @@ gosh() (
 
         #Check if the bookmark with this name already exists.
         bookmark_exists $name
-        if [[ $? == 0 ]]; then 
+        if [[ $? == 0 ]]; then
             print_error "Bookmark (${name}) already exists";
             fatal 1;
         fi
@@ -152,7 +206,7 @@ gosh() (
             print_error "Cannot add a bookmark (${name}) - path is invalid (${value})"
             fatal 1;
         fi
-        
+
         #Everything is ok...
         #Add the bookmark
         echo "${name} ${BOOKMARK_SEP} ${value}" >> $PATH_FILE_RC
@@ -168,11 +222,11 @@ gosh() (
 
         #Check if the bookmark with this name exists.
         bookmark_exists $name
-        if [[ $? != 0 ]]; then 
+        if [[ $? != 0 ]]; then
             print_error "Bookmark (${name}) does not exists";
             fatal 1;
         fi
-        
+
         #Search and print the inverse into a temp file.
         #Next move the temp file into the rc file.
         grep -v "${name}" "${PATH_FILE_RC}" > "${PATH_TEMP_FILE}";\
@@ -197,7 +251,7 @@ gosh() (
         name=$1;
         #Check if we have a bookmark with this name.
         bookmark_exists $name
-        if [[ $? != 0 ]]; then 
+        if [[ $? != 0 ]]; then
             print_error "Bookmark (${name}) does not exists";
             fatal 1;
         fi
@@ -209,15 +263,16 @@ gosh() (
     ############################################################################
     ## Initialization                                                         ##
     ############################################################################
-    while getopts "hva:r:u:lg" o; do
+    while getopts "hva:r:u:lgn" o; do
         case "${o}" in
             h) help_arg="true"      ;;
             v) version_arg="true"   ;;
             a) add_arg=${OPTARG}    ;;
             r) remove_arg=${OPTARG} ;;
             u) update_arg=${OPTARG} ;;
-            l) list_arg="true"      ;;         
+            l) list_arg="true"      ;;
             g) go_arg="true"        ;;
+            n) no_colors_arg="true" ;;
         esac
     done
     shift $((OPTIND-1))
@@ -232,21 +287,26 @@ gosh() (
         show_version; exit 0;
     fi;
 
+    ##No Colors.
+    if [ -n "${no_colors_arg}" ]; then
+        NO_COLORS=true;
+    fi;
+
     #ADD
-    if [ -n "${add_arg}" ]; then 
+    if [ -n "${add_arg}" ]; then
         add $add_arg $@; exit 0
     fi;
     #REMOVE
     if [ -n "${remove_arg}" ]; then
-        remove $remove_arg; exit 0;     
+        remove $remove_arg; exit 0;
     fi;
     #UPDATE
     if [ -n "${update_arg}" ]; then
         update $update_arg $@; exit 0;
     fi;
     #LIST
-    if [ -n "${list_arg}" ]; then        
-        list; exit 0; 
+    if [ -n "${list_arg}" ]; then
+        list; exit 0;
     fi;
     #GO
     if [ -n "${go_arg}" ]; then
@@ -256,7 +316,7 @@ gosh() (
 
 gosh-go() {
 
-    if [ -n "${1}" ]; then         
+    if [ -n "${1}" ]; then
         path=$(gosh -g $1);
         cd $path;
     else
